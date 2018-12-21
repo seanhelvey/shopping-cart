e20093d043dc0e7cd0cb37f3cbc5c47765f3485d
seanhelvey <sean.helvey@gmail.com>
2018-12-21 10:03:29 -0700

    add CartItems component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import CartItems from "./components/CartItems"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header/>
+        <CartItems/>
         <Footer/>
       </div>
     );
---
+++ src/components/CartItems.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const CartItems = () => {
+    return (
+        <div className="container">
+        <h1>Items</h1>
+        </div>
+    );
+}
+
+export default CartItems;

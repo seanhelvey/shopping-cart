72d54cdeef579c60512f51e29635aa963c17fe61
seanhelvey <sean.helvey@gmail.com>
2018-12-21 09:56:56 -0700

    add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const Footer = () => {
+    return (
+        <nav className="navbar navbar-dark bg-dark">
+        <a className="navbar-brand" href="#">&copy; 2018</a>
+        </nav>
+    );
+}
+
+export default Footer;

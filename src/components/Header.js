82e7409e2c4716b4c274b4f002c1011bde6f7c13
seanhelvey <sean.helvey@gmail.com>
2018-12-21 09:50:45 -0700

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+        <nav className="navbar navbar-dark bg-primary">
+        <a className="navbar-brand" href="#">Shopping Cart</a>
+        </nav>
+    );
+  }
+}
+
+export default App;

a612ee58deb9033180f15fff6227a2517b7b81f3
seanhelvey <sean.helvey@gmail.com>
2018-12-21 13:19:00 -0700

    AddItemComponent bootstrappy, but not working yet

--- src/App.js
+++ src/App.js
@@ -3,8 +3,27 @@ import './App.css';
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import CartItems from "./components/CartItems"
+import AddItem from "./components/AddItem"
 
 class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      products: [
+        { id: 40, name: 'Mediocre Iron Watch', priceInCents: 399 },
+        { id: 41, name: 'Heavy Duty Concrete Plate', priceInCents: 499 },
+        { id: 42, name: 'Intelligent Paper Knife', priceInCents: 1999 },
+        { id: 43, name: 'Small Aluminum Keyboard', priceInCents: 2500 },
+        { id: 44, name: 'Practical Copper Plate', priceInCents: 1000 },
+        { id: 45, name: 'Awesome Bronze Pants', priceInCents: 399 },
+        { id: 46, name: 'Intelligent Leather Clock', priceInCents: 2999 },
+        { id: 47, name: 'Ergonomic Bronze Lamp', priceInCents: 40000 },
+        { id: 48, name: 'Awesome Leather Shoes', priceInCents: 3990 },
+      ]
+    }
+  }
+
   render() {
     var cartItemsList = [
       { id: 1, product: { id: 40, name: 'Mediocre Iron Watch', priceInCents: 399 }, quantity: 1 },
@@ -16,6 +35,7 @@ class App extends Component {
       <div className="App">
         <Header/>
         <CartItems cartItemsList={cartItemsList}/>
+        <AddItem/>
         <Footer copyright={2016}/>
       </div>
     );
---
+++ src/components/AddItem.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const AddItem = (props) => {
+    console.log(props)
+    return (
+        <div className="container">
+        <form>
+        <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+        <div class="form-group">
+            <label for="exampleFormControlSelect1">Example select</label>
+            <select class="form-control" id="exampleFormControlSelect1">
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+            </select>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+        </div>
+    );
+}
+
+export default AddItem;
